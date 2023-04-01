55dd1e87b46a03a36786b425b5dcffd591356104
xeno <gxenogiannopoulos@uniwa.gr>
2023-04-01 18:33:43 +0300

    test_post_labels

--- machine_learning/tests.py
+++ machine_learning/tests.py
@@ -1,9 +1,12 @@
 import json
+import os
 
 from django.contrib.auth.models import User
 from rest_framework import status
 from rest_framework.reverse import reverse
 from rest_framework.test import APITestCase
+
+from medisp_project.settings import HIST_IMAGES
 
 
 # Create your tests here.
@@ -16,19 +19,39 @@ class TestLabel(APITestCase):
         )
         self.client.force_authenticate(user=self.user)
 
-    def test_post_label(self):
-        post_data = dict(name="Benign")
+    def test_post_labels(self):
+        new_label_name = "Benign"
+        post_data = dict(name=new_label_name)
         create_response = self.client.post(
-            path=reverse("label-list"),
+            path=reverse("labels-list"),
             content_type="application/json",
             data=json.dumps(post_data),
         )
         self.assertEqual(create_response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(
+            create_response.data, create_response.data | {"name": new_label_name}
+        )
 
-        post_data = dict(name="Malignant")
-        create_response = self.client.post(
-            path=reverse("label-list"),
-            content_type="application/json",
-            data=json.dumps(post_data),
-        )
+        MALIGNANT = "Malignant"
+        post_data = dict(name=MALIGNANT)
+        create_response = self.client.post(path=reverse("labels-list"), data=post_data)
         self.assertEqual(create_response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(
+            create_response.data, create_response.data | {"name": MALIGNANT}
+        )
+
+
+class TestHistImageModelViewset(APITestCase):
+    def setUp(self) -> None:
+        self.user = User.objects.create_user(
+            username="test_user", password="test_user", email="test@user.com"
+        )
+        self.client.force_authenticate(user=self.user)
+
+    def test_register_images(self):
+        expected_response = [
+            file for file in os.listdir(os.path.join(HIST_IMAGES, "train"))
+        ]
+        response = self.client.post(reverse("hist-images-register-images"))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, expected_response)
